bd96bb107958d90c85bfe72ac91b3f1d68565c59
Hachem MEDDEB <hachemmeddeb89@gmail.com>
2023-03-30 23:27:09 +0100

    changed the listening port

--- app.js
+++ app.js
@@ -162,6 +162,6 @@ app.get("/about", (req, res) => {
 });
 
 //--------------STARTING SERVER-------------\\
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server started on port 3000");
 });
